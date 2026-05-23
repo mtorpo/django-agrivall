6b28fa309007442fedbb94c510fbec54d5b3c09d
mtor <marctp14@gmail.com>
2026-05-23 19:30:37 +0200

    message when pedido finished

--- agrivall/views/views.py
+++ agrivall/views/views.py
@@ -13,6 +13,8 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
+from django.contrib import messages # para alertas
+
 
 def index(request):
     return render(request, "index.html")
@@ -97,7 +99,12 @@ def checkout(request):
             pedido.total = total
             pedido.estado = 'confirmado'
             pedido.save() # ahora si se guarda en la bdd, directamente sobre el objeto
-            return redirect('pedido_confirmado')
+            messages.success(
+                request,
+                "Pedido confirmado correctamente 🌱"
+            )
+
+            return redirect('productos')
     else:
         # Aquí se piden los datos para crear un pedido y generar el formulario
         form = PedidoForm(instance=cart)
