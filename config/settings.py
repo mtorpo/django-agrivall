f2e5a01169ce8f35652752365bc7d9af4faa0e24
mtor <marctp14@gmail.com>
2026-06-06 13:15:27 +0200

    unify settings for pro

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8@8(j=y$67%+$5!@tlx*yudd#2@j105cc5&l_g4=dhjqcw3ug'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "212.227.98.78",
+    "localhost",
+    "127.0.0.1",
+    "daw-agrivall.es",
+    "www.daw-agrivall.es",
+]
 
 
 # Application definition
@@ -125,7 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # PARA EL ALMACENAMIENTO DE LAS IMÁGENES DE LOS PRODUCTOS
 MEDIA_URL = '/media/'
